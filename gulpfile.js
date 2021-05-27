a8552666aad4f0f724e45083ad92d40ab820a460
Daniil <laito.treasure@gmail.com>
2021-05-27 13:45:14 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+const {src, dest, watch, parallel} = require('gulp');
+
+const scss = require('gulp-sass');
+
+const concat = require('gulp-concat');
+
+const browserSync = require('browser-sync').create();
+
+const uglify = require('gulp-uglify-es').default;
+
+const postcss = require('postcss-sort-media-queries');
+
+ function postcss([
+    sortMediaQueries({
+      sort: 'desktop-first'
+    })
+  ]) {
+    process(css);
+  }
+
+function browsersync() {
+  browserSync.init({
+    server: {
+      baseDir: 'app/'
+    }
+  });
+}
+
+function styles() {
+  return src('app/scss/style.scss')
+  .pipe(scss({outputStyle: 'compressed'}))
+  .pipe(concat('style.min.css'))
+  .pipe(dest('app/css'))
+  .pipe(browserSync.stream())
+}
+
+function watching() {
+  watch(['app/scss/**/*.scss'], styles);
+  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
+  watch(['app/*.html']).on('change', browserSync.reload)
+}
+
+function scripts() {
+  return src([
+    'node_modules/jquery/dist/jquery.js',
+    'app/js/main.js'
+  ])
+  .pipe(concat('main.min.js'))
+  .pipe(uglify())
+  .pipe(dest('app/js'))
+  .pipe(browserSync.stream())
+}
+
+exports.styles = styles;
+exports.watching = watching;
+exports.browsersync = browsersync;
+exports.scripts = scripts;
+exports.postcss = postcss;
+
+exports.default = parallel(postcss, scripts, browsersync, watching);
